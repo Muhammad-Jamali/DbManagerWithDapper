5012ea600a3183926de031a3a9d845fdaf550f49
Muhammad Jamali <Muhammad Jamali>
2022-05-13 17:28:55 +0500

    Added QueryWithoutTransaction

---
+++ DbManager/DbManagerFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace DbManager
+{
+    public static class DbManagerFactory
+    {
+        public static DbManager CreateInstance(string connectionString)
+        {
+            return new DbManager(connectionString);
+        }
+    }
+}
---
+++ DbManager/IDbManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace DbManager
+{
+    public interface IDbManager : IDisposable
+    {
+        public IDbConnection GetConnection();
+        public IDbTransaction GetTransaction();
+        public void OpenConnection();
+        public void BeginTransaction();
+        public void CommitTransaction();
+
+        public void CommitTransactionAndDispose();
+        public void RollbackTransaction();
+        public void RollbackTransactionAndDispose();
+
+        public void DisposeConnection();
+
+        public Task<IEnumerable<T>> QueryListAsync<T>(string sql, DynamicParameters parameters = null,
+            CommandType commandType = CommandType.StoredProcedure);
+
+        public Task<IEnumerable<T>> QueryListWithOutTransactionAsync<T>(string sql, DynamicParameters parameters = null,
+            CommandType commandType = CommandType.StoredProcedure);
+
+        public IEnumerable<T> QueryListWithOutTransaction<T>(string sql, DynamicParameters parameters = null,
+            CommandType commandType = CommandType.StoredProcedure);
+
+        public IEnumerable<T> QueryList<T>(string sql, DynamicParameters parameters = null,
+            CommandType commandType = CommandType.StoredProcedure);
+
+        public Task<T> QueryAsync<T>(string sql, DynamicParameters parameters = null,
+            CommandType commandType = CommandType.StoredProcedure);
+
+        public Task<T> QueryWithOutTransactionAsync<T>(string sql, DynamicParameters parameters = null,
+            CommandType commandType = CommandType.StoredProcedure);
+
+        public T QueryWithOutTransaction<T>(string sql, DynamicParameters parameters = null,
+            CommandType commandType = CommandType.StoredProcedure);
+
+        public T Query<T>(string sql, DynamicParameters parameters = null,
+            CommandType commandType = CommandType.StoredProcedure);
+
+        public Task<bool> ExecuteAsync(string sp, DynamicParameters parameters = null,
+            CommandType commandType = CommandType.StoredProcedure);
+    }
+}
